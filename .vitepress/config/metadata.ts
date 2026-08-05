bf11ac958da31f77eba0782a50d185bb74df70a8
Chinahackernet <chinahackernet@gmail.com>
2026-08-05 17:20:41 +0400

    Update metadata.ts

--- .vitepress/config/metadata.ts
+++ .vitepress/config/metadata.ts
@@ -1,6 +1,6 @@
 export const metaData = {
     lang: 'zh-CN',
     locale: 'zh_CN',
-    title: 'Silence's Blog',
+    title: "Silence's Blog",
     description: '赛伦斯,Silence,网络渗透,Gopher,热爱技术,中度强迫症,熬夜达人',
 }
